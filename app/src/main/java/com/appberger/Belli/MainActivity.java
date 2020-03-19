9da347fbe9d06bc41728c8831ba78ebb2296993e
Beau Berger <beauwberger@gmail.com>
2020-03-18 22:51:34 -0500

    Set text via app!

--- app/src/main/java/com/appberger/Belli/MainActivity.java
+++ app/src/main/java/com/appberger/Belli/MainActivity.java
@@ -1,9 +1,7 @@
 package com.appberger.Belli;
 
-import android.app.ActivityManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -13,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,11 +21,21 @@ import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.appberger.mjpegviewer.MjpegView;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import io.particle.android.sdk.cloud.ParticleCloud;
+import io.particle.android.sdk.cloud.ParticleCloudSDK;
+import io.particle.android.sdk.cloud.ParticleDevice;
+import io.particle.android.sdk.cloud.exceptions.ParticleCloudException;
+import io.particle.android.sdk.utils.Async;
+import io.particle.android.sdk.utils.Toaster;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private MjpegView view1;
-
+    public ParticleDevice mDevice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +47,38 @@ public class MainActivity extends AppCompatActivity {
         view1.setUrl("http://192.168.1.60:8080/video");
         view1.setRecycleBitmap(true);
         createNotificationChannel();
+        ParticleCloudSDK.init(this);
 
+        //******
+        //LOGIN
+        //******
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+
+
+            @Override
+            public Object callApi(@NonNull ParticleCloud sparkCloud) throws ParticleCloudException, IOException {
+                sparkCloud.logIn("beauwberger@gmail.com", "Widji123ji");
+                sparkCloud.getDevices();
+                try {
+                    mDevice = sparkCloud.getDevices().get(0);
+                } catch (IndexOutOfBoundsException iobEx) {
+                    throw new RuntimeException("Your account must have at least one device for this example app to work");
+                }
+                return -1;
+            }
+            @Override
+            public void onSuccess(@NonNull Object value) {
+                Toaster.l(MainActivity.this, "Logged in");
+                Toaster.l(MainActivity.this, mDevice.getID());
+            }
+
+            @Override
+            public void onFailure(@NonNull ParticleCloudException e) {
+                //Toaster.l(MainActivity.this, e.getBestMessage());
+                e.printStackTrace();
+                Log.d("info", e.getBestMessage());
+            }
+        });
         // [START retrieve_current_token]
         FirebaseInstanceId.getInstance().getInstanceId()
                 .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
@@ -113,5 +154,37 @@ public class MainActivity extends AppCompatActivity {
             if (notificationManager!=null){
                 notificationManager.createNotificationChannel(channel);}
         }
+    }
+    /** Called when the user touches the button */
+    public void sendText(View view) throws ParticleCloudException {
 
-}}
+        // "someDevice" is a ParticleDevice instance
+        Async.executeAsync(mDevice, new Async.ApiWork<ParticleDevice, Integer>() {
+
+            public Integer callApi(ParticleDevice particleDevice)
+                    throws ParticleCloudException, IOException {
+                ArrayList<String> args = new ArrayList<>();
+                EditText text = (EditText)findViewById(R.id.displayText);
+                args.add(text.getText().toString());
+                try {
+                    int resultCode = mDevice.callFunction("setText", args);
+                    Toaster.l(MainActivity.this, "Result of calling sendText: " + resultCode);
+                    return resultCode;
+                } catch (ParticleDevice.FunctionDoesNotExistException e) {
+                    e.printStackTrace();
+                }
+                return -1;
+            }
+
+            @Override
+            public void onSuccess(Integer integer) {
+                Toaster.l(MainActivity.this, "Success");
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                Toaster.l(MainActivity.this, exception.toString());
+            }
+        });
+    }
+}
